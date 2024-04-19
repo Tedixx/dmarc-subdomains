0e23f8fdb6554407328ae358261ced91db90e3f2
Tedix <82908532+Tedixx@users.noreply.github.com>
2024-04-19 10:02:15 +0200

    Include parsing of subdomains under 'Get All' button
    Thanks to @xnl-h4ck3r, dmarc-subdomains.py now includes subdomains which are hidden under the 'Get All' button at dmarc.live.

--- dmarc-subdomains.py
+++ dmarc-subdomains.py
@@ -11,18 +11,26 @@ async def fetch_dmarc_domains(domain):
         browser = await p.chromium.launch()
         page = await browser.new_page()
         await page.goto(f'https://dmarc.live/info/{domain}')
+
+        # Check for the presence of the 'Get All' button and click if available
+        button_selector = 'button:text("Get All")'
+        if await page.query_selector(button_selector):
+            await page.click(button_selector)
+            # Wait for any dynamic content to load after clicking the button
+            await page.wait_for_load_state('networkidle')
+
         content = await page.content()
         await browser.close()
-    
+
     soup = BeautifulSoup(content, 'html.parser')
     dmarc_domains = []
     h3_element = soup.find('h3', string=lambda text: 'Same DMARC Record' in text)
-    
+
     if h3_element:
         p_sibling = h3_element.find_next_sibling('p')
         if p_sibling:
             dmarc_domains = [link.get_text(strip=True) for link in p_sibling.find_all('a')]
-    
+
     return dmarc_domains
 
 async def main(domain):
